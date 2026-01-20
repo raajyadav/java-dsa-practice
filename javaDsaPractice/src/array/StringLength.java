2cb42b5a831f58e9956c5b52772574bffd912e6b
Raj Yadav <raajyadavnp@gmail.com>
2026-01-21 00:15:41 +0530

    string length

---
+++ javaDsaPractice/src/array/StringLength.java
@@ -0,0 +1,15 @@
+package array;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+
+
+		String[] str = {"Raj","Mohan", "Nitesh","Tauleshwar"};
+		
+		for(String x : str) {
+			System.out.println(x+" length is: "+x.length());
+		}
+	}
+
+}
